51cd05497ac1eb4da767cae9b7158f4efc7a3a1f
Joseph Abdul-Razzak <jrazzak01@gmail.com>
2025-06-26 03:48:37 +0100

    Comment rewording

--- NormaliseRoleColours.plugin.js
+++ NormaliseRoleColours.plugin.js
@@ -35,7 +35,7 @@ module.exports = class NormaliseRoleColours {
                 normaliseGradient(node);
             }
 
-            // scan its descendants
+            // normalise its descendants
             node.querySelectorAll?.(selector).forEach(normaliseGradient);
         }
 
